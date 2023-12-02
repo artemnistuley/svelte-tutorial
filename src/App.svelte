7ea9c16c609166f915c9ff648823f5bea6aa50e6
Artem Nistuley <artem.nistuley@gmail.com>
2023-12-02 12:11:10 +0200

    styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script>
   import FirstComponent from "./lib/basic/FirstComponent.svelte";
   import DynamicAttr from "./lib/basic/DynamicAttr.svelte";
+  import Styling from "./lib/basic/Styling.svelte";
 </script>
 
 <main>
   <!-- <FirstComponent /> -->
 
-  <DynamicAttr />
+  <!-- <DynamicAttr /> -->
+
+  <Styling />
 </main>
 
 <style></style>
---
+++ src/lib/basic/Styling.svelte
@@ -0,0 +1,11 @@
+<div>
+  <p>This is a paragraph.</p>
+</div>
+
+<style>
+  p {
+    font-size: 20px;
+    font-family: 'Comic Sans MS';
+    color: goldenrod;
+  }
+</style>
