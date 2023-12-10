46ee2c0fc768678248ca53ddfc76f89479f3a817
Artem Nistuley <artem.nistuley@gmail.com>
2023-12-11 00:19:24 +0200

    by section

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,18 @@
 <script>
-  import FirstComponent from "./lib/basic/FirstComponent.svelte";
-  import DynamicAttr from "./lib/basic/DynamicAttr.svelte";
-  import Styling from "./lib/basic/Styling.svelte";
-  import Nested from "./lib/basic/Nested.svelte";
-  import HtmlTag from "./lib/basic/HtmlTag.svelte";
-  import ReactivityAssignment from "./lib/basic/ReactivityAssignment.svelte";
-  import ReactivityDeclaration from "./lib/basic/ReactivityDeclaration.svelte";
-  import ReactivityStatement from "./lib/basic/ReactivityStatement.svelte";
-  import ReactivityObjects from "./lib/basic/ReactivityObjects.svelte";
-  import DeclaringProps from "./lib/basic/DeclaringProps.svelte";
-  import SpreadProps from "./lib/basic/SpreadProps.svelte";
-  import IfElseBlocks from "./lib/basic/IfElseBlocks.svelte";
-  import EachBlocks from "./lib/basic/EachBlocks.svelte";
-  import KeyedEachBlock from "./lib/basic/KeyedEachBlock.svelte";
+  import FirstComponent from "./lib/basic/intro/FirstComponent.svelte";
+  import DynamicAttr from "./lib/basic/intro/DynamicAttr.svelte";
+  import Styling from "./lib/basic/intro/Styling.svelte";
+  import Nested from "./lib/basic/intro/Nested.svelte";
+  import HtmlTag from "./lib/basic/intro/HtmlTag.svelte";
+  import ReactivityAssignment from "./lib/basic/reactivity/ReactivityAssignment.svelte";
+  import ReactivityDeclaration from "./lib/basic/reactivity/ReactivityDeclaration.svelte";
+  import ReactivityStatement from "./lib/basic/reactivity/ReactivityStatement.svelte";
+  import ReactivityObjects from "./lib/basic/reactivity/ReactivityObjects.svelte";
+  import DeclaringProps from "./lib/basic/props/DeclaringProps.svelte";
+  import SpreadProps from "./lib/basic/props/SpreadProps.svelte";
+  import IfElseBlocks from "./lib/basic/logic/IfElseBlocks.svelte";
+  import EachBlocks from "./lib/basic/logic/EachBlocks.svelte";
+  import KeyedEachBlock from "./lib/basic/logic/KeyedEachBlock.svelte";
 </script>
 
 <main>
